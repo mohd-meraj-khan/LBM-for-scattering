a2035016a7e810ea7ca0c13931476f742fb071fc
meraj <am19d041@smail.iitm.ac.in>
2024-06-27 17:03:23 +0530

    geometry module updated

--- Codes/OpenMP/ModuleGeometry.py
+++ Codes/OpenMP/ModuleGeometry.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #################################################################
 
-def carToPolar(r, phi, Nz=1, Ny=1, Nx=1, cy=0, cx=0):
+def carToPolar(r, phi, Nz=1, Ny=10, Nx=10, cy=0, cx=0):
     for i in range(Ny):
         for j in range(Nx):
             r[0, i, j] = np.sqrt((i - cy)**2 + (j - cx)**2)
@@ -15,38 +15,45 @@ def carToPolar(r, phi, Nz=1, Ny=1, Nx=1, cy=0, cx=0):
 
 #################################################################
 
-def circle(r, phi, er, er2, a, Nz=1, Ny=1, Nx=1):
+def circle(r, a, Nz=1, Ny=10, Nx=10):
+    inside = np.zeros((Nz, Ny, Nx), dtype=bool)
     for i in range(Ny):
         for j in range(Nx):
-            if (r[0, i, j] <= a):
-                er[0, i, j]  = er2
+            if r[0, i, j] <= a:
+                inside[0, i, j] = True
+    return inside
 #################################################################
 
 
 
 
+
 #################################################################
 
-def square(er, er2, a, Nz=1, Ny=1, Nx=1, cy=0, cx=0):
+def square(a, Nz=1, Ny=10, Nx=10, cy=0, cx=0):
+    inside = np.zeros((Nz, Ny, Nx), dtype=bool)
     for i in range(Ny):
         for j in range(Nx):
             Y = i - cy
             X = j - cx
             if (X <= a and X >= -a and Y <= a and Y >= -a):
-                er[0, i, j]  = er2
+                inside[0, i, j] = True
+    return inside
 #################################################################
 
 
 
 #################################################################
 
-def hexagon(er, er2, a, Nz=1, Ny=1, Nx=1, cy=0, cx=0):
+def hexagon(a, Nz=1, Ny=10, Nx=10, cy=0, cx=0):
+    inside = np.zeros((Nz, Ny, Nx), dtype=bool)
     for i in range(Ny):
         for j in range(Nx):
             Y = i - cy
             X = j - cx
             if (Y <= X + 2*a and Y >= - X - 2*a and Y >= X - 2*a and Y <= -X + 2*a and X >= - a and X <= a):
-                er[0, i, j]  = er2
+                inside[0, i, j] = True
+    return inside
 #################################################################   
 
 
@@ -54,11 +61,13 @@ def hexagon(er, er2, a, Nz=1, Ny=1, Nx=1, cy=0, cx=0):
 
 #################################################################
 
-def corrugatedEllipse(r, phi, er, er2, a, Nz=1, Ny=1, Nx=1, A=1, N=0, epsilon=0):
+def corrugatedEllipse(r, phi, a, Nz=1, Ny=10, Nx=10, A=1, N=0, epsilon=0):
+    inside = np.zeros((Nz, Ny, Nx), dtype=bool)
     for i in range(Ny):
         for j in range(Nx):
             if (r[0, i, j] <= a * (1 / ((np.cos(phi[0, i, j]))**2 + (A * np.sin(phi[0, i, j]))**2) + epsilon * np.cos(N * phi[0, i, j]))):
-                er[0, i, j]  = er2
+                inside[0, i, j] = True
+    return inside
 #################################################################
 
                 
@@ -66,7 +75,7 @@ def corrugatedEllipse(r, phi, er, er2, a, Nz=1, Ny=1, Nx=1, A=1, N=0, epsilon=0)
 
 #################################################################
 
-def JanusHalf(r, er, a, theta, er2, er3, Nz=1, Ny=1, Nx=1, cy=0, cx=0):
+def JanusHalf(r, er, a, theta, er2, er3, Nz=1, Ny=10, Nx=10, cy=0, cx=0):
     for i in range(Ny):
         for j in range(Nx):
             Y = i - cy
@@ -98,7 +107,7 @@ def JanusHalf(r, er, a, theta, er2, er3, Nz=1, Ny=1, Nx=1, cy=0, cx=0):
 
 #################################################################
 
-def JanusSegment(r, er, a, er2, er3, Nz=1, Ny=1, Nx=1, cy=0, cx=0, shift=0):
+def JanusSegment(r, er, a, er2, er3, Nz=1, Ny=10, Nx=10, cy=0, cx=0, shift=0):
     for i in range(Ny):
         for j in range(Nx):
             Y = i - cy
@@ -114,7 +123,7 @@ def JanusSegment(r, er, a, er2, er3, Nz=1, Ny=1, Nx=1, cy=0, cx=0, shift=0):
 
 #################################################################
 
-def JanusSector(r, er, a, er2, er3, Nz=1, Ny=1, Nx=1, cy=0, cx=0, theta=0):
+def JanusSector(r, er, a, er2, er3, Nz=1, Ny=10, Nx=10, cy=0, cx=0, theta=0):
     for i in range(Ny):
         for j in range(Nx):
             Y = i - cy
